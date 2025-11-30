7de8afab5c693696803c352c4c1a4b34ae3a1289
NgHuuDu <huuduit1907@gmail.com>
2025-11-30 19:57:19 +0700

    Update Program.cs

--- DormitoryManagementSystem.API/Program.cs
+++ DormitoryManagementSystem.API/Program.cs
@@ -66,11 +66,10 @@ builder.Services.AddScoped<IStatisticsBUS, StatisticsBUS>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseCors("AllowAll");
 
