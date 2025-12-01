5a518187f7ebf5068f84a1057af7246aa0f31128
HuyMinh <nguyenminhhuy595@gmail.com>
2025-12-01 17:22:54 +0700

    Translate exception into VNese

--- DormitoryManagementSystem.DTO/Students/StudentContactUpdateDTO.cs
+++ DormitoryManagementSystem.DTO/Students/StudentContactUpdateDTO.cs
@@ -11,15 +11,16 @@ namespace DormitoryManagementSystem.DTO.Students
     {
         [Required(ErrorMessage = "Số điện thoại không được để trống")]
         [Phone(ErrorMessage = "Định dạng số điện thoại không hợp lệ")]
+        [StringLength(11, ErrorMessage = "Số điện thoại không được quá 11 ký tự")]
         public string PhoneNumber { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Email không được để trống")]
-        [EmailAddress(ErrorMessage = "Định dạng email không hợp lệ")]
+        [EmailAddress(ErrorMessage = "Định dạng Email không hợp lệ")]
+        [StringLength(100, ErrorMessage = "Email không được quá 100 ký tự")]
         public string Email { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Địa chỉ không được để trống")]
-        [StringLength(250, ErrorMessage = "Địa chỉ không được vượt quá 250 ký tự")]
+        [StringLength(255, ErrorMessage = "Địa chỉ không được quá 255 ký tự")]
         public string Address { get; set; } = string.Empty;
-
     }
 }
